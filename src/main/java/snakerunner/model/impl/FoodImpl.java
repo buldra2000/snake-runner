577fd1cfb746b40da4588ceb49e6017d5b3514ad
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-01-20 16:01:33 +0100

    added Food interface

--- src/main/java/snakerunner/model/impl/FoodImpl.java
+++ src/main/java/snakerunner/model/impl/FoodImpl.java
@@ -5,6 +5,7 @@ import snakerunner.model.FoodEffect;
 import snakerunner.model.Snake;
 
 public class FoodImpl implements Food{
+    
     private FoodEffect effect;
 
     public FoodImpl(FoodEffect effect) {
