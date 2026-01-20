b93ee7e840051c569737035a2ca4cfbb9640b8c5
Betul Ince <betul@192.168.1.7>
2026-01-20 22:42:40 +0100

    added initial setup for obstacle and position

--- src/main/java/snakerunner/model/Obstacle.java
+++ src/main/java/snakerunner/model/Obstacle.java
@@ -2,4 +2,15 @@ package snakerunner.model;
 
 public class Obstacle {
     
+    //Obstacle's position
+    private Position position;
+
+    public Obstacle(Position position){
+
+        this.position= position;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
 }
