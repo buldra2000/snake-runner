3e08e250690dea4833977f0625e01032f5bfec22
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-01-22 17:58:29 +0100

    Added new function for the grid in level implementation

--- src/main/java/snakerunner/model/impl/LevelImpl.java
+++ src/main/java/snakerunner/model/impl/LevelImpl.java
@@ -1,18 +1,24 @@
 package snakerunner.model.impl;
 
+import snakerunner.model.Grid;
 import snakerunner.model.Level;
 
 public class LevelImpl implements Level{
     // GRANDEZZA LIVELLO
-    // private final int width;
-    // private final int height;
+    private final Grid grid;
     private final int baseSpeed;
     private final int ItemCount;
     private final int levelDuration;
 
-    public LevelImpl(int baseSpeed, int itemCount, int levelDuration) {
+    public LevelImpl(Grid grid, int baseSpeed, int itemCount, int levelDuration) {
+        this.grid = grid;
         this.baseSpeed = baseSpeed;
         this.ItemCount = itemCount;
         this.levelDuration = levelDuration;
+    }
+
+    @Override
+    public Grid getGrid() {
+        return grid;
     }
 }
