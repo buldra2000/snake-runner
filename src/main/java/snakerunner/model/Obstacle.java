0e8dac40297257fd8667334c2daee779cfe7cf17
Betul Ince <betul@Mac.station>
2026-01-24 00:16:56 +0100

    Added collision control

--- src/main/java/snakerunner/model/Obstacle.java
+++ src/main/java/snakerunner/model/Obstacle.java
@@ -13,9 +13,13 @@ public class Obstacle extends Entity{
 
     }
 
-//Checking 
-    public boolean isHit(){
-
+//We check if the obstacle is hit by the snake
+    public boolean isHit(int snakeX, int snakeY){
+        //If obstacle's and snake's coordinates are the same then this means that there is a collision.
+ if (this.x==snakeX && this.y==snakeY){
+    return true;
+ }
+//... else it's false
     return false;       
 
     
