0975d2c0808c7779e44fad2d7195548b1f3390c2
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-01-26 15:47:56 +0100

    fixed gamemodel interface

--- src/main/java/snakerunner/model/GameModel.java
+++ src/main/java/snakerunner/model/GameModel.java
@@ -13,17 +13,10 @@ public interface GameModel {
     public void resetLevel();
 
     public void nextLevel();
+
     public void startTimer();
 
     public void stopTimer();
 
     public int getTimeLeft();
-
-    public boolean isGameOver();
-
-    public void loadLevel(Level level);
-
-    public void resetLevel();
-
-    public void nextLevel();
 }
