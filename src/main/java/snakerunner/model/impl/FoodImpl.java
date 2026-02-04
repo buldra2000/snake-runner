24ed7641d6f2ed40c2b12d5dd0ddfa7e62ca636a
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-02-04 19:35:24 +0100

    Integrated the new loader and collectibles in game model

--- src/main/java/snakerunner/model/impl/FoodImpl.java
+++ src/main/java/snakerunner/model/impl/FoodImpl.java
@@ -2,21 +2,18 @@ package snakerunner.model.impl;
 
 import snakerunner.commons.Point2D;
 import snakerunner.model.Collectible;
-import snakerunner.model.FoodEffect;
 import snakerunner.model.Snake;
 
 public class FoodImpl implements Collectible{
-    private final FoodEffect effect;
     private final Point2D<Integer, Integer> position;
 
-    public FoodImpl(FoodEffect effect, Point2D<Integer, Integer> position){
+    public FoodImpl(Point2D<Integer, Integer> position){
         this.position = position;
-        this.effect = effect;
     }
 
     @Override
     public void consume(Snake snake) {
-        effect.apply(snake);
+        // Implement consumption logic here
     }
 
     @Override
