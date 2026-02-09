338509007144cd8bc9d5a502420212cf2965ca40
Betul Ince <betul@192.168.1.51>
2026-02-09 20:38:03 +0100

    Fixed some things in the html file

--- src/main/java/snakerunner/model/Door.java
+++ src/main/java/snakerunner/model/Door.java
@@ -2,15 +2,14 @@ package snakerunner.model;
 
 import snakerunner.commons.Point2D;
 
-
 /* Doors can be open when the user collects the key */
 public class Door extends Obstacle {
 
     private boolean open; //Checking if the door is open
 
-    public Door(int x, int y){
-        super(x, y, 1, 1); //Default size is 1x1
-        this.open= false; //At the beginning the door is closed
+    public Door(final int x, final int y) {
+        super(x, y, 1, 1); /*Default size is 1x1*/
+        this.open = false; //At the beginning the door is closed
 
     }
 
@@ -20,12 +19,12 @@ public class Door extends Obstacle {
     }
 
     //Setter
-    public void setOpen(boolean open){
-        this.open= open;
+    public void setOpen(final boolean open) {
+        this.open = open;
     }
 
     //Getting door's positions
-    public Point2D<Integer,Integer> getPosition(){
+    public Point2D<Integer,Integer> getPosition() {
     return new Point2D<Integer,Integer>(getX(), getY());
     }
 
